3a703694c8e5d96ce7923e07dea46a653245e78d
kyrich333 <richky@student.sgu.ac.id>
2026-03-09 14:18:04 +0700

    backend wiring and restructuring index as entry point, routes as address for functions, controllers containing function

---
+++ server/controllers/inventoryController.js
@@ -0,0 +1,28 @@
+// contains inventory function to get inventory data from database and send it as response to client
+
+
+const pool = require("../db/db");
+
+exports.getInventory = async (req, res) => {
+  try {
+    const result = await pool.query(`
+      SELECT
+        items.id,
+        item_types.code,
+        brands.name AS brand,
+        locations.code AS location,
+        inventory.quantity
+      FROM inventory
+      JOIN items ON inventory.item_id = items.id
+      JOIN item_types ON items.item_type_id = item_types.id
+      JOIN brands ON items.brand_id = brands.id
+      JOIN locations ON inventory.location_id = locations.id
+    `);
+
+    res.json(result.rows);
+
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Server error");
+  }
+};
--- server/index.js
+++ server/index.js
@@ -2,25 +2,16 @@ require("dotenv").config();
 
 const express = require("express");
 const cors = require("cors");
-const pool = require("./db");
 
+const inventoryRoutes = require("./routes/inventoryRoute");
+console.log(inventoryRoutes);
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.get("/", async (req, res) => {
-  try {
-    const result = await pool.query("SELECT NOW()");
-    res.json({
-      message: "Server running",
-      database_time: result.rows[0].now
-    });
-  } catch (err) {
-    console.error(err);
-    res.status(500).send("Database connection error");
-  }
-});
+app.use("/inventory", inventoryRoutes);
+
 
 const PORT = process.env.PORT || 5000;
 
